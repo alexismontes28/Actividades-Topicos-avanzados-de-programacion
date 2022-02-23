7ceb45872427eaa2afc1bf68706f8a1f893acccd
alexismontes28 <90781230+alexismontes28@users.noreply.github.com>
2022-02-23 11:53:04 -0700

    Add files via upload

---
+++ ACTIVIDAD_4_SEMANA_2/src/actividad_4_semana_2/ACTIVIDAD_4_SEMANA_2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad_4_semana_2;
+
+/**
+ *
+ * @author laex2
+ */
+public class ACTIVIDAD_4_SEMANA_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hola mundo");
+    }
+    
+}
